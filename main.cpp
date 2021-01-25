79ddbb749582a7a4566e900eb7cb7ffa7e77c919
paulo-rozatto <rozatto.paulo.victor@gmail.com>
2021-01-25 11:09:21 -0300

    adcionando arquivos enviados pelo Stenio

--- main.cpp
+++ main.cpp
@@ -1,51 +1,257 @@
 #include <iostream>
 #include <fstream>
+#include <string>
+#include <math.h>
+#include <utility>
+#include <tuple>
+#include <iomanip>
+#include <stdlib.h>
+#include <chrono>
+#include "Graph.h"
+#include "Node.h"
 
 using namespace std;
 
-void imprimeArquivoDeEntrada(string nomeArquivo)
+Graph *leitura(ifstream &input_file, int directed, int weightedEdge, int weightedNode)
 {
-    ifstream arquivo;
-    string texto;
 
-    arquivo.open(nomeArquivo); // tenta abrir um arquivo de texto
+    //Variáveis para auxiliar na criação dos nós no Grafo
+    int idNodeSource;
+    int idNodeTarget;
+    int order;
 
-    if(arquivo.is_open() && arquivo.good()) // verifica se abertura do arquivo deu certo
+    //Pegando a ordem do grafo
+    input_file >> order;
+
+    //Criando objeto grafo
+    Graph *graph = new Graph(order, directed, weightedEdge, weightedNode);
+
+    //Leitura de arquivo
+
+    if (!graph->getWeightedEdge() && !graph->getWeightedNode())
     {
 
-        while (getline (arquivo, texto))
+        while (input_file >> idNodeSource >> idNodeTarget)
         {
-            cout << texto << endl;
-        }
 
-        arquivo.close();
+            graph->insertEdge(idNodeSource, idNodeTarget, 0);
+        }
     }
-    else
+    else if (graph->getWeightedEdge() && !graph->getWeightedNode())
     {
-        cout << "Nenhum arquivo de texto valido encontrado." << endl;
+
+        float edgeWeight;
+
+        while (input_file >> idNodeSource >> idNodeTarget >> edgeWeight)
+        {
+
+            graph->insertEdge(idNodeSource, idNodeTarget, edgeWeight);
+        }
+    }
+    else if (graph->getWeightedNode() && !graph->getWeightedEdge())
+    {
+
+        float nodeSourceWeight, nodeTargetWeight;
+
+        while (input_file >> idNodeSource >> nodeSourceWeight >> idNodeTarget >> nodeTargetWeight)
+        {
+
+            graph->insertEdge(idNodeSource, idNodeTarget, 0);
+            graph->getNode(idNodeSource)->setWeight(nodeSourceWeight);
+            graph->getNode(idNodeTarget)->setWeight(nodeTargetWeight);
+        }
+    }
+    else if (graph->getWeightedNode() && graph->getWeightedEdge())
+    {
+
+        float nodeSourceWeight, nodeTargetWeight, edgeWeight;
+
+        while (input_file >> idNodeSource >> nodeSourceWeight >> idNodeTarget >> nodeTargetWeight)
+        {
+
+            graph->insertEdge(idNodeSource, idNodeTarget, edgeWeight);
+            graph->getNode(idNodeSource)->setWeight(nodeSourceWeight);
+            graph->getNode(idNodeTarget)->setWeight(nodeTargetWeight);
+        }
+    }
+
+    return graph;
+}
+
+Graph *leituraInstancia(ifstream &input_file, int directed, int weightedEdge, int weightedNode)
+{
+
+    //Variáveis para auxiliar na criação dos nós no Grafo
+    int idNodeSource;
+    int idNodeTarget;
+    int order;
+    int numEdges;
+
+    //Pegando a ordem do grafo
+    input_file >> order >> numEdges;
+
+    //Criando objeto grafo
+    Graph *graph = new Graph(order, directed, weightedEdge, weightedNode);
+
+    //Leitura de arquivo
+    while (input_file >> idNodeSource >> idNodeTarget)
+    {
+
+        graph->insertEdge(idNodeSource, idNodeTarget, 0);
+    }
+
+    return graph;
+}
+
+int menu()
+{
+
+    int selecao;
+
+    cout << "MENU" << endl;
+    cout << "----" << endl;
+    cout << "[1] Subgrafo induzido por conjunto de vértices" << endl;
+    cout << "[2] Caminho Mínimo entre dois vértices - Dijkstra" << endl;
+    cout << "[3] Caminho Mínimo entre dois vértices - Floyd" << endl;
+    cout << "[4] Árvore Geradora Mínima de Prim" << endl;
+    cout << "[5] Árvore Geradora Mínima de Kruskal" << endl;
+    cout << "[6] Imprimir caminhamento em largura" << endl;
+    cout << "[7] Imprimir ordenacao topológica" << endl;
+    cout << "[8] Algoritmo Guloso" << endl;
+    cout << "[9] Algoritmo Guloso Randomizado " << endl;
+    cout << "[10] Algoritmo Guloso Randomizado Reativo" << endl;
+    cout << "[0] Sair" << endl;
+
+    cin >> selecao;
+
+    return selecao;
+}
+
+void selecionar(int selecao, Graph *graph, ofstream &output_file)
+{
+
+    switch (selecao)
+    {
+
+    //Subgrafo induzido por um conjunto de vértices X;
+    case 1:
+    {
+
+        break;
+    }
+        //Caminho mínimo entre dois vértices usando Dijkstra;
+    case 2:
+    {
+
+        break;
+    }
+
+        //Caminho mínimo entre dois vértices usando Floyd;
+    case 3:
+    {
+
+        break;
+    }
+
+        //AGM - Kruscal;
+    case 4:
+    {
+
+        break;
+    }
+
+        //AGM Prim;
+    case 5:
+    {
+
+        break;
+    }
+
+        //Busca em largura;
+    case 6:
+    {
+
+        break;
+    }
+        //Ordenação Topologica;
+    case 7:
+    {
+
+        break;
+    }
+    default:
+    {
+        cout << " Error!!! invalid option!!" << endl;
+    }
     }
 }
 
-/**
-* @param argc - quantidade de argumentos passado para main via terminal de linha de comando
-* @param *argv[] - vetor de char que contém os argumentos, um para cada string passada na linha de comando.
-*
-* argv[0] armazena o nome do programa que foi chamado no prompt, sendo assim, argc é pelo menos igual a 1, pois no mínimo existirá um argumento.
-*/
-int main(int argc, char* argv[])
+int mainMenu(ofstream &output_file, Graph *graph)
 {
-    cout << argc << " argumentos passados.\n";
 
-    for (int i = 0; i < argc; ++i)
+    int selecao = 1;
+
+    while (selecao != 0)
     {
-        cout << argv[i] << "\n";
+        system("clear");
+        selecao = menu();
+
+        if (output_file.is_open())
+            selecionar(selecao, graph, output_file);
+
+        else
+            cout << "Unable to open the output_file" << endl;
+
+        output_file << endl;
     }
 
-    if(argc > 1)
+    return 0;
+}
+
+int main(int argc, char const *argv[])
+{
+
+    //Verificação se todos os parâmetros do programa foram entrados
+    if (argc != 6)
     {
-        cout << "\nTentando abrir " << argv[1] << endl;
-        imprimeArquivoDeEntrada(argv[1]);
+
+        cout << "ERROR: Expecting: ./<program_name> <input_file> <output_file> <directed> <weighted_edge> <weighted_node> " << endl;
+        return 1;
     }
+
+    string program_name(argv[0]);
+    string input_file_name(argv[1]);
+
+    string instance;
+    if (input_file_name.find("v") <= input_file_name.size())
+    {
+        string instance = input_file_name.substr(input_file_name.find("v"));
+        cout << "Running " << program_name << " with instance " << instance << " ... " << endl;
+    }
+
+    //Abrindo arquivo de entrada
+    ifstream input_file;
+    ofstream output_file;
+    input_file.open(argv[1], ios::in);
+    output_file.open(argv[2], ios::out | ios::trunc);
+
+    Graph *graph;
+
+    if (input_file.is_open())
+    {
+
+        graph = leituraInstancia(input_file, atoi(argv[3]), atoi(argv[4]), atoi(argv[5]));
+    }
+    else
+        cout << "Unable to open " << argv[1];
+
+    mainMenu(output_file, graph);
+
+    //Fechando arquivo de entrada
+    input_file.close();
+
+    //Fechando arquivo de saída
+    output_file.close();
 
     return 0;
 }
